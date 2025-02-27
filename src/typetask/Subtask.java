f29b88211e2d6e34860922df482c49ab8d388765
PETRO <dim4petro357@yandex.ru>
2025-02-27 11:17:47 +0800

    Checkstyle

--- src/typetask/Subtask.java
+++ src/typetask/Subtask.java
@@ -7,6 +7,7 @@ public class Subtask extends Task {
         super(status,description,name);
         this.epic = epic;
     }
+    
     public Subtask(Status status, String description, String name, int id) {
         super(status,description,name);
         this.setId(id);
