541f4b7f89e8c31688f9f9268b84e02ae66f2a95
PETRO <dim4petro357@yandex.ru>
2025-02-27 11:35:29 +0800

    checkstyle6

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -4,8 +4,6 @@ import typetask.Epic;
 import typetask.Status;
 import typetask.Subtask;
 import typetask.Task;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
